a145b528a169125f1d8a7818cb287ddb4d4db274
David Riordan <dr@daveriordan.com>
2016-12-05 17:45:32 -0500

    Restores Copyfile module

---
+++ copyfile.py
@@ -0,0 +1,34 @@
+import os
+import shutil
+import logging
+
+
+def touch(fname, times=None):
+    fpath, f = os.path.split(fname)
+    if not os.path.exists(fpath):
+        os.makedirs(fpath)
+
+    with open(fname, 'a'):
+        os.utime(fname, times)
+
+def copyFile(src, dest):
+    #Src Exists?
+    try:
+        if os.path.isfile(src):
+            dpath, dfile = os.path.split(dest)
+
+            if not os.path.isdir(dpath):
+                os.makedirs(dpath)
+
+            if not os.path.exists(dest):
+                touch(dest)
+            try:
+                shutil.copy2(src, dest)
+            # eg. src and dest are the same file
+            except shutil.Error as e:
+                logging.exception('Error: %s' % e)
+            # eg. source or destination doesn't exist
+            except IOError as e:
+                logging.exception('Error: %s' % e.strerror)
+    except:
+        logging.exception('Error: src to copy does not exist.')
